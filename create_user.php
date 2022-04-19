81139aeba3576cb4784f2b649dfc5d7ca0119c4e
Becky Lin <beckylin123@ku.edu>
2022-04-19 10:30:04 -0500

    https://people.eecs.ku.edu/~b884l228/

---
+++ create_user.php
@@ -0,0 +1,26 @@
+<?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "b884l228", "einahH7a", "b884l228");
+$username = $_POST["username"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+$user = "INSERT INTO users (username, password, email) VALUES (\"$username\", \"$password\", \"$email\");";
+if ($mysqli->query($user)== TRUE)
+{
+    echo'<script>
+            alert("Account created successfully");
+            location.href = "https://people.eecs.ku.edu/~b884l228/"
+        </script>';
+    
+}
+else
+{
+    echo "Error<br>";
+}
+    
+
+$mysqli->close();
+?>
