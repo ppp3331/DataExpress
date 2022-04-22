b192e16b6f67bfa49d441af102925530f49f62d7
codemagic4ever <83933499+codemagic4ever@users.noreply.github.com>
2022-04-22 15:27:25 -0500

    still trying to make changes

--- home.php
+++ home.php
@@ -3,13 +3,13 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
+<body>
 <style>
         body
         {
             background: linear-gradient(155deg, #38F9BA 35%, cyan);
         }
 </style>
-</head>
+    </body>
 
 </html>
