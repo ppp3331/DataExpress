f873f53721aaa86a55cab357a508cbfab95b8e91
Becky Lin <beckylin123@ku.edu>
2022-05-02 17:19:14 -0500

    shabiwanyier

--- log_in.php
+++ log_in.php
@@ -3,27 +3,32 @@ session_start();
 $mysqli = new mysqli("mysql.eecs.ku.edu", "b884l228", "einahH7a", "b884l228");
 $username = $_POST["username"];
 $password = $_POST["password"];
-$email = $_POST["email"];
+
 if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
-$user = "SELECT username, password FROM users where username='$username' AND password='$password';";
+$user = "SELECT username, password, email FROM users where username='$username' AND password='$password';";
 $result = $mysqli->query($user);
+if ($result->num_rows>0)
+{
+    $_SESSION['username']= $username; 
+    $r=$result->fetch_assoc();
+    $_SESSION['email']=$r['email'];
+    $_SESSION['password']=$password;
 
-if ($result->num_rows > 0) {
-    $_SESSION['username'] = $username;
-    $_SESSION['email'] = $email;
-    $_SESSION['password'] = $password;
-    echo '<script>
-            location.href = "https://people.eecs.ku.edu/~b884l228/home.php"
+    echo'<script>
+            location.href = "https://people.eecs.ku.edu/~b884l228/team.php"
         </script>';
-} else {
-    echo '<script>
+}
+else
+{
+    echo'<script>
             confirm("Invalid username or password");
             location.href = "https://people.eecs.ku.edu/~b884l228/"
         </script>';
 }
-
+    
 
 $mysqli->close();
+?>
---
+++ team.php
@@ -0,0 +1,103 @@
+<?php
+session_start();
+$mysqli = new mysqli("mysql.eecs.ku.edu", "b884l228", "einahH7a", "b884l228");
+        if ($mysqli->connect_errno) {
+            printf("Connect failed: %s\n", $mysqli->connect_error);
+            exit();
+        }
+        
+
+?>
+
+<html>
+
+<body style="background-color:#f0f4f7;">
+    <header >
+        <style>
+            #a:hover{
+                color:blue;
+            }
+            #div1{
+                border-radius: 15px; border-color: #c9cfd4; border-style: solid; text-align: center; border-width: 2;
+                    background-color: white;width:300px; height:430px;
+            }
+            #text1{
+                font-size: 20; font-weight: 500;
+            }
+            #div2{
+                margin-bottom: 10;color: #706f6f;
+            }
+            #div3{
+                text-align:left; margin-left: 30;margin-right: 15;
+            }
+        </style>
+        
+    </header>
+    <div style="text-align: left; margin-left: 1300; position:absolute">
+        <text> Profile &nbsp; </text>
+        <text onclick="location.href='logout.php'" id="a"> Log out</text>
+    </div>
+    <br><br>
+    <h1 style="text-align: center;">Select Your Team</h1><br>
+    <div id="wrap">
+    
+        <?php
+        $email = $_SESSION['email'];
+        $query= "SELECT name, description FROM team, join1 WHERE  join1.team_ID=team.team_ID AND join1.email='$email'" ;
+        $result = $mysqli->query($query);
+        if ($result->num_rows>0)
+        {
+            while ($row = $result->fetch_assoc())
+            {
+                $tname=$row["name"];
+                $query1="SELECT username from users, join1, team where join1.team_ID=team.team_ID and join1.email=users.email and name='$tname'";
+                $result1 = $mysqli->query($query1);
+                if ($result1->num_rows>0)
+                {
+                    echo "<div id='div1'> <br><text id='text1'>". $row["name"]."</text><br><br><br>
+                        <div class='block'>
+                        <div style='text-align:left; margin-left: 30;'>
+                        <text>Member:</text>
+                        </div>";
+                        while ($row1 = $result1->fetch_assoc())
+                    {
+                        echo "<div style='margin-bottom: 10;color: #706f6f;'><br><text>".$row1["username"]. "</text></div>";
+                        
+                    }
+                        echo "<br><div id='div3'> <text>Description:</text><br>
+                        <text style='color: #706f6f;'>
+                        &nbsp; &nbsp;&nbsp;&nbsp;". $row["description"]. "</text></div></div><br><button>Join</button></div>";
+                     
+                }
+            
+                
+            }
+        }
+
+        ?>
+        <div id="div4" style="border-radius: 15px; border-color: #c9cfd4; border-style: solid; text-align: center; border-width: 2;
+                    background-color: white;width:300px; height:430px;">
+            <br><text style="font-size: 20">
+                Create a new team
+            </text><br>
+            <br><br><br><br><br><br>
+            <a href="createNewTeam.html">
+                <img src="icons8-add.gif" width="100" alt="addicon">
+            </a>
+        </div>
+    </div>
+</body>
+<style>
+    #wrap {
+        display: flex;
+        justify-content: space-around;
+    }
+
+    .block {
+        height: 270px;
+        width: 285px;
+        overflow: hidden;
+        overflow-y: auto;
+    }
+</style>
+<html>
