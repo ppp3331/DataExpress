feae69c86141a4b299afe721cb0db48e28f7fc58
codemagic4ever <83933499+codemagic4ever@users.noreply.github.com>
2022-05-02 21:54:40 -0500

    profile changes

--- profile.php
+++ profile.php
@@ -2,22 +2,26 @@
 session_start();
 $mysqli = new mysqli("mysql.eecs.ku.edu", "b884l228", "einahH7a", "b884l228");
 $username = $_POST["username"];
-//$password = $_POST["password"];
+$password = $_POST["password"];
 $email = $_POST["email"];
 if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
-$user = "SELECT username, email FROM users where username='$username' AND email='$email';";
+$user = "SELECT username, email, team_ID FROM users, join1 where username='$username' AND email='$email' AND teamID='$team_ID' AND users.email = join1.email;";
 $result = $mysqli->query($user);
 
 if ($result->num_rows > 0) {
     $_SESSION['username'] = $username;
     $_SESSION['email'] = $email;
+    $_SESSION['teamID'] = $team_ID;
     //$_SESSION['password'] = $password;
     echo '<script>
             location.href = "https://people.eecs.ku.edu/~b884l228/profile.html"
         </script>';
+    echo $username;
+    echo $email;
+    echo $team_ID;
 } 
 
 
