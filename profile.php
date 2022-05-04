643789cbfd0162f5fa39ed60e96df0fd23476804
codemagic4ever <83933499+codemagic4ever@users.noreply.github.com>
2022-05-04 02:32:32 -0500

    printing teams on profile page

--- profile.php
+++ profile.php
@@ -27,7 +27,7 @@ $mysqli = new mysqli("mysql.eecs.ku.edu", "b884l228", "einahH7a", "b884l228");
     $username = $_SESSION['username'];
     $email = $_SESSION['email'];
     $team_ID = $_SESSION['team_ID'];
-    $user = "SELECT username, email, team_ID FROM users, join1 WHERE username='$username' AND users.email='$email' AND join1.team_ID='$team_ID' AND users.email = join1.email;";
+    $user = "SELECT team_ID FROM join1, users WHERE team_ID='$team_ID' AND users.email = join1.email;";
     $result = $mysqli->query($user);
     
     //echo '<script>
@@ -42,10 +42,10 @@ $mysqli = new mysqli("mysql.eecs.ku.edu", "b884l228", "einahH7a", "b884l228");
     <h3> Email: <?php echo $email;?> </h3>
     <div style="text-align: left; margin-left: 1300; position:absolute">
     <br><text onclick="location.href='reset.html'" id="a"> Reset Password</text><br>
-    <!-- <h3> Team: 
-        <?php //while($row = $result -> fetch_assoc()){
-        //echo $row["team_ID"];}?> 
-    </h3>  -->
+    <h3> Team(s): 
+        <?php while($row = $result -> fetch_assoc()){
+        echo $row["team_ID"];}?> 
+    </h3> 
     
     <!-- echo "<br>";
     echo $email;
